f7efcb5a94308197e2e59533ea35901d5aa58c20
shantanu <shantanuchavhan10@gmail.com>
2023-09-13 11:20:43 +0530

    commit

--- api/app.js
+++ api/app.js
@@ -143,6 +143,7 @@ app.post('/logout', (req, res) => {
         const parts = originalname.split('.');
         const ext = parts[parts.length - 1];
         const newPath = path + '.' + ext;
+        console.log(newPath,"iuoih")
         fs.renameSync(path, newPath);
         uploadedFiles.push(newPath.replace('uploads/', ''));
   
