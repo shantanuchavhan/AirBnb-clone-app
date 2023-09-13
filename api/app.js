0763f76f5b83938a688a302384d4611921c886a7
shantanu <shantanuchavhan10@gmail.com>
2023-09-13 10:28:26 +0530

    commit

--- api/app.js
+++ api/app.js
@@ -46,11 +46,8 @@ const storage = multer.diskStorage({
   filename: (req, file, cb) => {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
     cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
-    console.log("photosUploading")
   },
 });
-
-const photosMiddleWare = multer(storage);
 
 
 
@@ -114,7 +111,7 @@ app.post('/logout', (req, res) => {
     });
   });
 
- 
+  const photosMiddleWare = multer({ dest: 'uploads' });
 
   app.post('/allListing',async(req,res)=>{
     
