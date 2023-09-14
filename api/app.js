2c8b5c7d273b19961a03fed1017658bfe5da8b29
shantanu <shantanuchavhan10@gmail.com>
2023-09-14 07:36:00 +0530

    commit

--- api/app.js
+++ api/app.js
@@ -187,19 +187,19 @@ app.post('/logout', (req, res) => {
   });
 
 
-  app.post("/upload", upload.single("my_file"), async (req, res) => {
-    try {
-      const b64 = Buffer.from(req.file.buffer).toString("base64");
-      let dataURI = "data:" + req.file.mimetype + ";base64," + b64;
-      const cldRes = await handleUpload(dataURI);
-      res.json(cldRes);
-    } catch (error) {
-      console.log(error);
-      res.send({
-        message: error.message,
-      });
-    }
-  });
+  // app.post("/upload", upload.single("my_file"), async (req, res) => {
+  //   try {
+  //     const b64 = Buffer.from(req.file.buffer).toString("base64");
+  //     let dataURI = "data:" + req.file.mimetype + ";base64," + b64;
+  //     const cldRes = await handleUpload(dataURI);
+  //     res.json(cldRes);
+  //   } catch (error) {
+  //     console.log(error);
+  //     res.send({
+  //       message: error.message,
+  //     });
+  //   }
+  // });
 
 
   
